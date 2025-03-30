2590e5347319f79581a6bfc5c9aa164e723bc8e6
Mateus Castro <mateus.castro@labsit.io>
2025-03-30 15:42:33 -0300

    add flight routes profile

---
+++ BestFlight.WebAPI/Mapping/FlightRoutesProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BestFlight.Application.DTOs;
+using BestFlight.Domain.Models;
+
+namespace BestFlight.API.Mapping
+{
+    public class FlightRoutesProfile : Profile
+    {
+        public FlightRoutesProfile()
+        {
+            CreateMap<FlightRoutesDto, FlightRoutes>()
+                .ReverseMap();
+        }
+    }
+}
